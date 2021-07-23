ca1c5f3327e8826841c1c849beb9ab7d861fcc07
Muhammad Luthfi Azzammi <luthfia911@gmail.com>
2021-07-23 21:04:30 +0700

    AddNewBook Completed

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const {addNewBookHandler} = require('./handler/addNewBook');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addNewBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: () => {},
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: () => {},
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
